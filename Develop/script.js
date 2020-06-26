d5fbd95362363de9dbc44261b88057ac363a5b6f
Royce Williams <roycejames1130@gmail.com>
2020-06-25 20:52:28 -0700

    added the storing in local storage function and the check local storage on load function

--- Develop/script.js
+++ Develop/script.js
@@ -5,6 +5,15 @@ $(document).ready(function () {
     console.log(moment().format("h:mm:ss"));
     $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
     colorTable();
+    $(document).on("click",".saveBtn",function(){
+        var timePeriod = $(this).val()
+        console.log(timePeriod);
+        var task = document.querySelector("#textarea"+timePeriod).value;
+        console.log(task);
+        localStorage.setItem(timePeriod,task);
+
+    })
+    checkStorage();
 });
 function colorTable(){
     console.log(moment().format("h a"));
@@ -21,13 +30,29 @@ function colorTable(){
         var timeSlot = $(targeter).data("value");
         console.log(timeSlot);
         if(current>parseInt(timeSlot)){
+            // $(targeter).addClass("past");
             $(targeter).parent().addClass("past");
+
         }
         if(current===parseInt(timeSlot)){
+            // $(targeter).addClass("present");
             $(targeter).parent().addClass("present");
+
         }
         if(current<parseInt(timeSlot)){
             $(targeter).parent().addClass("future");
+            // $(targeter).addClass("future");
+
         }
     }
+}
+
+// Submits a string with the value of the time window that is checked.
+function checkStorage(time){
+    for(var i =9;i<18; i ++){
+        var task = localStorage.getItem(i.toString());
+        $("#textarea"+i).val(task);
+    }
+    
+
 }
